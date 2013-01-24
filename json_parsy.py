4eebb57bf57b997fdc9fc073106a89197a764af1
Maria <mcm.guaba@gmail.com>
2013-01-23 23:12:10 -0800

    added html structure to content to fix encoding, other misc format fixes

--- json_parsy.py
+++ json_parsy.py
@@ -12,32 +12,38 @@ import datetime
 import time
 
 def main():
-	#read in data
-	json_unclean_string = open(sys.argv[1], 'rU')
-	clean_json(json_unclean_string)
-	json_data = json.load(json_unclean_string)
-	json_unclean_string.close()
-	#parse json
-	for data in json_data:
-		try:
-			t = time.strptime(data["modified"], "%Y-%m-%d %H:%M:%S")
-		except ValueError:
-			t = time.strptime(data["created"], "%Y-%m-%d %H:%M:%S")
-		file_path = os.path.join('content', str(t.tm_year), str(t.tm_mon), data["alias"]+'.html')
-		dir = os.path.dirname(file_path)
-		if not os.path.exists(dir):
-			os.makedirs(dir)
-		html_file = open(file_path , 'w')
-		html_file.write(data["fulltext"].encode('utf-8'))
-		html_file.close()
+  #read in data
+  json_unclean_file_obj = open(sys.argv[1], 'rU')
+  json_clean_string = clean_json(json_unclean_file_obj)
+  json_data = json.loads(json_clean_string)
+  json_unclean_file_obj.close()
+  #parse json
+  for data in json_data:
+    try:
+      t = time.strptime(data["modified"], "%Y-%m-%d %H:%M:%S")
+    except ValueError:
+	    t = time.strptime(data["created"], "%Y-%m-%d %H:%M:%S")
+    file_path = os.path.join('content', str(t.tm_year), str(t.tm_mon), data["alias"]+'.html')
+    dir = os.path.dirname(file_path)
+    if not os.path.exists(dir):
+	    os.makedirs(dir)
+    structured_html = add_head(data)
+    html_file = open(file_path , 'w')
+    html_file.write(structured_html.encode("utf-8"))
+    html_file.close()
 
 def clean_json(doc_text):
-	sarray = []
-	for line in doc_text :
-        	sarray.append(line.rstrip('\n\r\t'))
-   	output = open("clean.json", "w")
-	output.write(''.join(sarray))
-	#return ''.join(sarray)
+  sarray = []
+  for line in doc_text:
+    sarray.extend(line.split())
+  clean_string_json = " ".join(sarray)
+  new_clean_string = clean_string_json.replace("\'", "\\'")
+  return new_clean_string
+
+def add_head(html):
+  prepend = "<html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" /><title>" + html["title"] + "</title></head><body>"
+  h1 = "<h1>" + html["title"] + "</h1>"
+  return prepend + h1 + html["fulltext"] + "</body></html>"
 
 if __name__ == "__main__":
 	main()
