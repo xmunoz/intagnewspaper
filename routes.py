56eec4a81e83ee35313324185c20ea74bd172a2f
whroman3 <whroman3@gmail.com>
2013-10-04 01:46:45 -0700

    Began work on static design of page-head template and single-article template

--- routes.py
+++ routes.py
@@ -3,10 +3,14 @@ from flask import Flask, url_for, redirect, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def root():
+@app.route("/<int:year>/<int:month>/<title>")
+def root(year, month, title):
     # return render_template("adios.html")
-    return redirect( url_for ("static", filename="content/2012/1/adios.html") )
+    # return redirect( url_for ("static", filename= "content/2012/1/%s" %static_file  )
+    # static_file = static_file + ".html"
+    filepath = "static/content/%d/%d/%s.html" % (year, month, title)
+    return redirect(filepath)
+    # return redirect( filepath )
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+// window.onload = function() {
+//     $(".branch")
+//         .on("mouseover", function() {
+//             $(this).parent().addClass("rounded");
+//         })
+//         .on("mouseout", function() {
+//             $(this).parent().removeClass("rounded");
+//         })
+// }
