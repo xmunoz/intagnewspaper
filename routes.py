8d986ef72ee1065acfbbaadf6eef7206cb0025ca
whroman3 <whroman3@gmail.com>
2013-10-13 22:05:47 -0700

    * created templates for single article, article list and pdf archive * updated design with smaller headers and hover states on list pages and blog roll

--- routes.py
+++ routes.py
@@ -10,50 +10,28 @@ PASSWORD = 'pw'
 
 app = Flask(__name__)
 
-# connect to db
-# def connect_db():
-#     return sqlite3.connect(app.config['DATABASE'])
-
-# initialize db
-# def init_db():
-#     with closing (connect_db() ) as db:
-#         with app.open_resource("schema.sql", mode = "r") as f:
-#             db.cursor().executescript(f.read())
-#         db.commit()
-
-# current db connection is stored in "g" object
-# @app.before_request
-# def before_request():
-#     g.db = connect_db()
-
-# @app.teardown_request
-# def teardown_request(exception):
-#     db = getattr(g, "db", None)
-#     if db is not None:
-#         db.close()
-
-###
-
 @app.route("/")
 def homepage():
-    return render_template("homepage.html")
+    title = "Periodico Intag"
+    return render_template("homepage.html", title = title)
 
 @app.route("/articulos")
 def articulos():
-    return render_template("homepage.html")
+    title = "Periodico Intag | Articulos"
+    articles = "articles object"
+    return render_template("article_list.html", title = title, articles = articles)
 
 @app.route("/archivo")
 def archivo():
-    return render_template("homepage.html")
+    title = "Periodico Intag | Archivo"
+    archivo = "archivo object"
+    return render_template("homepage.html", title = title, archivo = archivo)
 
-@app.route("/articulos/<int:year>/<int:month>/<title>")
-def root(year, month, title):
-    # return render_template("adios.html")
-    # return redirect( url_for ("static", filename= "content/2012/1/%s" %static_file  )
-    # static_file = static_file + ".html"
-    filepath = "static/content/%d/%d/%s.html" % (year, month, title)
-    return redirect(filepath)
-    # return redirect( filepath )
+@app.route("/articulos/<article_title>")
+def root(article_title):
+    title = "Periodico Intag | " + article_title
+    article = "article object"
+    return render_template("article_single.html", title = title , article = article)
 
 if __name__ == "__main__":
     app.run(debug=True)
