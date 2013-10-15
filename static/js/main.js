e6cdf17dd17c360c3d1c8b246ebfc3b108cf994d
whroman3 <whroman3@gmail.com>
2013-10-14 23:57:58 -0700

    added hashtag js filtering logic for static articles page

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,54 @@
-// window.onload = function() {
-//     $(".branch")
-//         .on("mouseover", function() {
-//             $(this).parent().addClass("hover");
-//         })
-//         .on("mouseout", function() {
-//             $(this).parent().removeClass("hover");
-//         })
-// }
+var hash_toolkit = {},
+    default_year = "2012";
 
-// window.onload = function() {
-//     $(".menu-tab")
-//         .on("mouseover", function() {
-//             $(this).addClass("hover");
-//         })
-//         .on("mouseout", function() {
-//             $(this).removeClass("hover");
-//         })
-// }
+$(function() {
+    var onload_hash = window.location.hash,
+        set_year = default_year;
 
+    if ( onload_hash == "" ) {
+        window.location.hash = set_year; 
+    } else {
+        set_year = onload_hash.substring(1)
+    }
+
+    hash_toolkit = {
+        el  : null,
+        siblings    : null,
+        tab_el  : null,
+        tab_siblings    : null,
+        year_display_el    : $(".hash-year"),
+        year    : set_year
+    }
+
+    update_toolkit(hash_toolkit, set_year);
+
+    hash_action(hash_toolkit);
+})
+
+$(window).on("hashchange", function(ev) {
+    hash_toolkit.year = window.location.hash.substring(1)
+    console.log('asdasd')
+    update_toolkit( hash_toolkit, window.location.hash.substring(1) )
+
+    hash_action(hash_toolkit);
+})
+
+var hash_action = function(hash_obj) {
+    hash_obj.year_display_el.html( hash_obj.year )
+
+    console.log('action', hash_obj);
+
+    hash_obj.el.addClass("display");
+    hash_obj.siblings.removeClass("display");
+    hash_obj.tab_el.addClass("selected");
+    hash_obj.tab_siblings.removeClass("selected");
+
+}
+
+var update_toolkit = function (hash_obj, current_hash) {
+    hash_obj.el = $(".archive-loop[year='" + current_hash + "']");
+    hash_obj.siblings = $(".archive-loop:not([year='" + current_hash + "']");
+    hash_obj.tab_el = $(".menu-tab[year='" + current_hash + "']");
+    hash_obj.tab_siblings = $(".menu-tab:not([year='" + current_hash + "']");
+    hash_obj.year = current_hash
+}
