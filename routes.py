51452e55e46f8ce9b4b3c2268a4127c29ec43d77
Cristina <mcmguaba@gmail.com>
2013-10-21 20:57:59 -0700

    Replaced hard-coded links with url_for in templates
    *Added new favicon
    *Minor css fix to month in nav on article-single view
    *Routes refactor
    *Beginning of Babel integration

--- routes.py
+++ routes.py
@@ -1,34 +1,36 @@
-from flask import Flask, url_for, redirect, render_template, g
-from utils.common import get_index_data, get_article_full
+from flask import Flask, url_for, redirect, render_template, g, request
+from flask.ext.babel import Babel
+from src.common import get_index_data, get_article_full
 
 app = Flask(__name__)
+babel = Babel(app)
 
 @app.route("/")
 def homepage():
     title = "Periodico Intag"
     return render_template("homepage.html", title=title)
 
-@app.route("/articulos")
-def articulos():
-    articles = get_index_data('Article')
-    title = 'Articulos'
-    return render_template("index_page.html", type='article',
-            title=title, items=articles)
-
-@app.route("/archivo")
-def archivo():
-    title = "Archivo"
+@app.route("/archive")
+def archive():
+    title = "Archive"
     pdfs = get_index_data('PDF')
     body_class = "article-list"
     return render_template("index_page.html", type='pdf', title=title,
             items=pdfs, body_class=body_class)
 
-@app.route("/articulos/<article_alias>")
-def single_article(article_alias):
-    article = get_article_full(article_alias)
-    title = article.title + ' | Periodico Intag'
-    return render_template("article_single.html", title=title,
-            article=article)
+@app.route("/articles")
+@app.route("/articles/<article_alias>")
+def articles(article_alias=None):
+    if article_alias:
+        article = get_article_full(article_alias)
+        title = article.title + ' | Periodico Intag'
+        return render_template("article_single.html", title=title,
+                article=article)
+    else:
+        articles = get_index_data('Article')
+        title = 'Articles'
+        return render_template("index_page.html", type='article',
+                title=title, items=articles)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -37,6 +39,10 @@ def page_not_found(e):
 @app.errorhandler(500)
 def site_down(e):
     return render_template('500.html', error=e), 500
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(['en', 'es'])
 
 if __name__ == "__main__":
     app.run(debug=True)
