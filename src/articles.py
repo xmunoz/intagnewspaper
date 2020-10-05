6e3995862da7d9ba49c79e91d95b7f95718abc7f
Cristina Muñoz <hi@xmunoz.com>
2020-10-04 20:43:38 -0700

    upgrade everything

--- src/articles.py
+++ src/articles.py
@@ -1,20 +1,17 @@
-class Article:
-    """
-    Article object.
-    """
 
+class Article:
+    '''
+    Article object.
+    '''
     def __init__(self, alias):
         self.alias = alias
 
     def load_index_data(self, db):
         c = db.cursor()
-        c.execute(
-            "SELECT title, author, date, intro FROM articles WHERE alias LIKE '%s';",
-            (self.alias,),
-        )
+        c.execute("SELECT title, author, date, intro FROM articles WHERE alias LIKE '%s';" % self.alias)
         result = c.fetchone()
         self.title = result[0]
-        self.author = result[1]
+        self.author =result[1]
         self.date = result[2]
         self.intro = result[3]
         c.close()
@@ -22,7 +19,8 @@ class Article:
     def load_all_data(self, db):
         self.load_index_data(db)
         c = db.cursor()
-        c.execute("SELECT body FROM articles WHERE alias LIKE '%s';", (self.alias,))
+        c.execute("SELECT body FROM articles WHERE alias LIKE '%s';" % self.alias)
         result = c.fetchone()
         self.body = result[0]
         c.close()
+
--- src/pdfs.py
+++ src/pdfs.py
@@ -1,20 +1,19 @@
-class PDF:
-    """
-    Pdf object.
-    """
 
+class PDF:
+    '''
+    Pdf object.
+    '''
     def __init__(self, issue):
         self.issue = issue
 
     def load_index_data(self, db):
         c = db.cursor()
-        c.execute(
-            "SELECT headline, filename, thumbnail, date FROM pdfs WHERE issue_number = %d;",
-            (self.issue,),
-        )
+        c.execute("SELECT headline, filename, thumbnail, date FROM pdfs WHERE issue_number = %d;" % self.issue)
         result = c.fetchone()
         self.headline = result[0]
         self.filename = result[1]
         self.thumbnail_filename = result[2]
         self.date = result[3]
         c.close()
+
+
